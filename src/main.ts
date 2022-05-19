ee0196fe1ffa7a74c976230b0f2ccc0e8d7e4d0d
辣橙 <2625835752@qq.com>
2022-05-19 15:41:07 +0800

    05191536 优化main.ts代码结构

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
-import {createApp} from 'vue'
-import App from './App.vue'
-
-const app = createApp(App)
-
-// region plugins
+import {createApp} from "vue";
 import {createPinia} from "pinia";
 import {createI18n} from "vue-i18n";
 import {languages} from "./languages";
 import router from "./router";
+import App from "./App.vue";
 
-app.use(createPinia())
-
-app.use(createI18n({
-    legacy: false,
-    locale: "zh",
-    fallbackLocale: "en",
-    messages: languages
-}))
-
-app.use(router)
-// endregion
-
-app.mount('#app')
+createApp(App)
+    .use(createPinia())
+    .use(createI18n({
+        legacy: false,
+        locale: "zh",
+        fallbackLocale: "en",
+        messages: languages,
+    }))
+    .use(router)
+    .mount("#app");
