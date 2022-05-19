c54a51ad42959f5485adbfcb368cd04407eeeb5d
辣橙 <2625835752@qq.com>
2022-05-19 16:37:47 +0800

    05191637 CoursePc & CourseMobile 路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,43 @@
-import {createRouter, createWebHashHistory} from "vue-router";
+import {createRouter, createWebHashHistory, RouteRecordRaw} from "vue-router";
 import Index from "../pages/index/Index.vue";
 import About from "../pages/about/About.vue";
+import CourseMobile from "../pages/course/mobile/CourseMobile.vue";
+import CoursePc from "../pages/course/pc/CoursePc.vue";
 
+const THRESHOLD_WIDTH_OF_PC = 750;
 
-const routes = [
+const routes: RouteRecordRaw[] = [
     {
         path: "/",
         component: Index,
-        query: {},
         name: "index",
         alias: "/index/",
     },
     {
         path: "/about/",
         component: About,
-        query: {},
         name: "about",
+    },
+    {
+        path: "/course/",
+        component: CoursePc,
+        name: "course-pc",
+        alias: "/course/pc/",
+        beforeEnter(_, from) {
+            if (from.name === "course-mobile") {
+                // 如果是从手机端界面跳转过来的，则允许小屏幕设备访问`course-pc`
+                return true;
+            }
+            if (window.innerWidth <= THRESHOLD_WIDTH_OF_PC) {
+                // 屏幕宽度小于`THRESHOLD_WIDTH_OF_PC`时，跳转到手机端
+                return {name: "course-mobile"};
+            }
+        },
+    },
+    {
+        path: "/course/mobile/",
+        component: CourseMobile,
+        name: "course-mobile",
     },
 ];
 
