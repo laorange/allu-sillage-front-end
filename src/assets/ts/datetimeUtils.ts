5ef272fe0cd3fa7d5aebd9cc7a177ddc1f2c0429
辣橙 <2625835752@qq.com>
2022-05-19 17:39:41 +0800

    05191739 filterOptions getWeekAmountBetweenTwoDay

--- src/assets/ts/datetimeUtils.ts
+++ src/assets/ts/datetimeUtils.ts
@@ -1,5 +1,7 @@
 import dayjs from "dayjs";
 
+const SECONDS_OF_ONE_WEEK = 604800000;
+
 export function formatDate(d: dayjs.Dayjs) {
     return d.format("YYYY-MM-DD");
 }
@@ -15,4 +17,8 @@ export function formatDatetime(d: dayjs.Dayjs) {
 export function getIsoWeekDay(d: dayjs.Dayjs) {
     let day = d.day();
     return day === 0 ? 7 : day;
+}
+
+export function getWeekAmountBetweenTwoDay(from: dayjs.Dayjs = dayjs(), to: dayjs.Dayjs = dayjs()) {
+    return Math.floor(to.diff(from) / SECONDS_OF_ONE_WEEK);
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
 import {defineStore} from "pinia";
-import {formatDate} from "../assets/ts/timeFormater";
+import {formatDate, getIsoWeekDay, getWeekAmountBetweenTwoDay} from "../assets/ts/datetimeUtils";
 import {CourseApi} from "../assets/ts/api";
 import {ApiData} from "../assets/ts/types";
 import dayjs from "dayjs";
@@ -7,6 +7,15 @@ import dayjs from "dayjs";
 type State = {
     courseApi: CourseApi
     apiData: ApiData
+    filterOptions: {
+        date: dayjs.Dayjs
+        classrooms: number[]
+        groups: number[]
+        teacherName: string
+        method: string
+        week: number
+        whatDay: number
+    }
 }
 
 export const useStore = defineStore("store", {
@@ -17,7 +26,7 @@ export const useStore = defineStore("store", {
                 semesterConfig: {
                     "current_period": 30,
                     "current_period_display": "2022.02~2022.07",
-                    "week1_monday_date": "2022-02-28 00:00:00",
+                    "week1_monday_date": "2022-02-28T00:00:00",
                     "max_week": 20,
                 },
                 courses: [],
@@ -26,12 +35,31 @@ export const useStore = defineStore("store", {
                 courseChangeLogs: [],
                 classrooms: [],
             },
+            filterOptions: {
+                date: dayjs(),
+                classrooms: [],
+                groups: [],
+                teacherName: "",
+                method: "",
+                week: -1,
+                whatDay: getIsoWeekDay(dayjs()),
+            },
         };
     },
     getters: {},
     actions: {
         transformGradeToSemester(grade: number) {
             return grade * 2 - this.apiData.semesterConfig.current_period % 2;
+        },
+        initializeState() {
+            let weekNum = getWeekAmountBetweenTwoDay(dayjs(this.apiData.semesterConfig.week1_monday_date), dayjs()) + 1;
+            if (weekNum > this.apiData.semesterConfig.max_week) {
+                weekNum = this.apiData.semesterConfig.max_week;
+            }
+            if (weekNum <= 0) {
+                weekNum = 1;
+            }
+            this.filterOptions.week = weekNum;
         },
         async updateInfoFromBackend() {
             // 无需排队请求
@@ -64,6 +92,8 @@ export const useStore = defineStore("store", {
             );
             this.apiData.semesterConfig = (await this.courseApi.courseApiSemesterConfigGet()).data;
             this.apiData.courses = (await this.courseApi.courseApiCourseGet(`${this.apiData.semesterConfig.current_period}`)).data;
+
+            this.initializeState();
         },
     },
 });
