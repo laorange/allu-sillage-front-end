2c846c6f03b1efd6977deb5e4bf7a397cf508878
duang <929211182@qq.com>
2022-05-21 19:20:20 +0800

    05211920 a little left

--- src/pages/course/pc/CoursePc.vue
+++ src/pages/course/pc/CoursePc.vue
@@ -3,7 +3,7 @@ import {useRoute} from "vue-router";
 import {useStore} from "../../../store/store";
 import ToolBar from "../../../components/ToolBar.vue";
 import {ref} from "vue";
-import PcCourseTitle from "../../../components/Course/PcCourseTitle.vue";
+import PcCourseTitle from "../../../components/Course/pc/PcCourseTitle.vue";
 
 
 const route = useRoute();
@@ -20,6 +20,33 @@ const isBookmark = ref(true);
   <h2>semester = {{ semester }}</h2>
 </template>
 
-<style scoped>
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0 auto;
+}
 
+div[class^="line-row"] {
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  border: 1px solid black;
+  border-bottom: 0;
+}
+
+div[class^="line-col"] {
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  border: 1px solid black;
+  border-bottom: 0;
+}
+
+.date {
+  border-bottom: .5px solid gray;
+}
 </style>
