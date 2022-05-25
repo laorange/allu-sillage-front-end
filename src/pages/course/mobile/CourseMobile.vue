c3540db8d7ecf75e23d463a838d226b18aba776f
辣橙 <2625835752@qq.com>
2022-05-25 16:17:38 +0800

    perf(CourseMobile.vue): At the top of each lesson, the start and end times are added

--- src/pages/course/mobile/CourseMobile.vue
+++ src/pages/course/mobile/CourseMobile.vue
@@ -12,9 +12,35 @@ import DatePickerForWeekAndWhatDay from "../../../components/common/user-input/D
 import {filters} from "../../../assets/ts/api/filters";
 import {Course} from "../../../assets/ts/api";
 import {getIsoWeekDay} from "../../../assets/ts/datetimeUtils";
+import {useI18n} from "vue-i18n";
 
 const route = useRoute();
 const store = useStore();
+const {locale} = useI18n();
+
+const startAndEndTimeOfEachClass = {
+  fr: [
+    "08H00/09H35",
+    "10H05/11H40",
+    "13H30/15H05",
+    "15H35/17H10",
+    "18H30/20H05",
+  ],
+  en: [
+    "08:00-09:35",
+    "10:05-11:40",
+    "13:30-15:05",
+    "15:35-17:10",
+    "18:30-20:05",
+  ],
+  zh: [
+    "08:00~09:35",
+    "10:05~11:40",
+    "13:30~15:05",
+    "15:35~17:10",
+    "18:30~20:05",
+  ],
+};
 
 const semester = route.query.semester ?? -1;
 
@@ -49,16 +75,19 @@ onMounted(() => {
   <date-picker-for-week-and-what-day v-model:week="week" v-model:what-day="whatDay" :date="date"/>
 
   <div class="course-box-container" v-for="whichLesson in 5" :key="`whichLesson${whichLesson}`">
+    <n-divider>{{ startAndEndTimeOfEachClass[locale][whichLesson - 1] }}</n-divider>
     <course-box :courses="filteredCoursesInThisDay.filter(filters.course.byWhichLesson(whichLesson))"/>
   </div>
 </template>
 
 <style>
-.course-box {
-  margin-top: 10px;
-}
-
 .n-drawer .n-drawer-content .n-drawer-body-content-wrapper {
   padding: 16px 0;
+}
+
+.n-divider:not(.n-divider--vertical) {
+  margin-bottom: 0;
+  margin-top: 10px;
+  font-size: 12px;
 }
 </style>
